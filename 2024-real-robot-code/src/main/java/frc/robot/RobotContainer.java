2a0f98cf150a4eb91922afb88693abc35ad7b2cf
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-07-08 17:04:54 -0700

    use controller to move motor

--- 2024-real-robot-code/src/main/java/frc/robot/RobotContainer.java
+++ 2024-real-robot-code/src/main/java/frc/robot/RobotContainer.java
@@ -53,7 +53,8 @@ public class RobotContainer {
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(turnMotorButton(0.5));
+    
+   m_drivebase.setDefaultCommand(turnMotorButton(m_driverController.getLeftY()));
   }
 
   private Command turnMotorButton(double speed) {
